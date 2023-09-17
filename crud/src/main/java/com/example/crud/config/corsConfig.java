71aac7a3dc9a8f629364db46dfca34bc883ce077
shad9611 <elohim_shadai@hotmail.com>
2023-09-17 12:11:10 -0700

    change cors configurations
    change config to cors access

---
+++ crud/src/main/java/com/example/crud/config/corsConfig.java
@@ -0,0 +1,24 @@
+
+package com.example.crud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class corsConfig {
+
+  @Bean
+  public WebMvcConfigurer configurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE");
+      }
+    };
+  }
+
+}
